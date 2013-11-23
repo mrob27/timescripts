1e1e4dc561d30860f4e6d554bad22583910ff99d
Robert Munafo <mrob27@gmail.com>
2013-11-22 19:47:06 -0500

    Detect very light font colours and fix by changing background to black.

--- no-tinytext.user.js
+++ no-tinytext.user.js
@@ -3,7 +3,7 @@
 // @name No Tinytext
 // @description Locate tinytext and make it readable
 // @author Robert Munafo
-// @version 5848.50
+// @version 5949.74
 // @downloadURL http://mrob.com/time/scripts-beta/no-tinytext.user.js
 // @include http://forums.xkcd.com/viewtopic.php*
 // @include http://fora.xkcd.com/viewtopic.php*
@@ -17,21 +17,32 @@
 // np5832.36 get it (mostly) working.
 // np5848.50 more sophisticated size mapping, and defeat nested constructs
 //   (innermost size is the one that will take effect)
+// np5848.86 Add comment pointing to my sample post
+// np5949.74 Detect and fix very light font colors by changing the background
+//   to black.
 
-// A sample forum page is:
+// A sample forum post containing a variety of sizes, including Vytron's
+// nested super-size hack, is here:
 //
-//   http://forums.xkcd.com/viewtopic.php?f=7&t=101043&start=29360
+//   http://forums.xkcd.com/viewtopic.php?p=3448128#p3448128
 //
-// Tinytext appears within items like:
+// In the HTML served up by echochamber, tinytext appears within items like:
 //
-// <span style="font-size: 50%; line-height: 116%;">
-//   edit: spoilered
+// <span style="font-size: 20%; line-height: 116%;">
+//   This would normally be way too small to read
 // </span>
+//
+// I have trouble reading sizes below about 50%.
 
 var pat1 = new RegExp('^[0-9]\%$');   // Match '0%' through '9%'
 var pat2 = new RegExp('^[0-5][0-9]\%$');   // Match '00%' through '59%'
 var pat3 = new RegExp('^[0-9]+\%$');   // Match any number followed by '%'
 var pat4 = new RegExp('^[0-9]+');   // Match just a number at the beginning
+
+var pc1 = new RegExp('rgb\\([12][0-9][0-9], [12][0-9][0-9], [12][0-9][0-9]\\)'); // e.g. 'rgb(123, 234, 210)'
+var pc2 = new RegExp('rgb\\(([12][0-9][0-9]), ([12][0-9][0-9]), ([12][0-9][0-9])\\)'); // for use with .match()
+var gthres = 190;
+var pc3 = new RegExp('\#[A-F][0-9A-F][A-F][0-9A-F][A-F][0-9A-F]'); // e.g. '#BFFFFF' (best friends forever!)
 
 notinytext = {
 
@@ -64,6 +75,23 @@ notinytext = {
         }
         spans[i].style.fontSize = sz + "px";
       }
+
+      // Fix text that has a very light colour by changing the background to black
+      if ( pc2.test(spans[i].style.color) )
+      {
+        if ((RegExp.$1 > gthres) && (RegExp.$2 > gthres)
+         && (RegExp.$3 > gthres))
+        {
+          spans[i].style.backgroundColor = '#000';
+        }
+        this.log(i + ' ' + spans[i].style.color);
+      };
+      // Test color again using '#80BFFF' syntax
+      if ( pc3.test(spans[i].style.color) )
+      {
+        spans[i].style.backgroundColor = '#000';
+        this.log(i + ' ' + spans[i].style.color);
+      };
     }
   }
 };
