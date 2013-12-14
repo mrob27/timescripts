0707d854b2205fe77875f9d5841f38e82714af88
Robert Munafo <mrob27@gmail.com>
2013-12-13 23:56:00 -0500

    Add a few comments in the section that actually does the work

--- cake-time-styler.user.js
+++ cake-time-styler.user.js
@@ -12,6 +12,10 @@
 // @grant none
 // ==/UserScript==
 
+// The original was written in LiveScript and compiled into sorta-cryptic
+// slightly-obfuscated JavaScript. Formatting and comments are by Robert
+// Munafo (mrob27)
+//
 // REVISION HISTORY:
 //
 // 6457.14 copied from time.aasg.name/userscripts/CakeTimeStyler.user.js
@@ -20,10 +24,12 @@
 (function(){
   var a,b,c,d;
   a="101043",
+
   b=function(b){
     var c;
     c=document.querySelector("#topic-search input[name=t]").value,c===a&&b()
   },
+
   c=function(a,b,c){
     var d;
     return null==c&&(c=document),
@@ -31,6 +37,7 @@
             'contains(concat(" ", normalize-space(@class), " "), " $1 ")'),
       d=document.evaluate(a,c,null,b,null),
       b===XPathResult.FIRST_ORDERED_NODE_TYPE?d.singleNodeValue:d},
+
   d=function(a){
     var b,d,e,f;
     b=function(a,b){
@@ -39,19 +46,26 @@
         a.style.color=b,
         d=c('ancestor::*[@class="post"]',XPathResult.FIRST_ORDERED_NODE_TYPE,a),
         d.style.border="2px dotted "+b},
-        d=+a.textContent,
-        e=d%100,
+        d=+a.textContent, /* Post count */
+        e=d%100, /* Post count modulo 100, i.e. the last 2 digits of the post count */
         a=a.parentElement,
-        0===e?b(a,"#859900"):
-         e>=97?b(a,"#dc322f"):
-           3>=e&&b(a,"#268bd2"),
-        1190===d&&(f=c('ancestor::*[@class="postprofile"]//img[@alt="User avatar"]',XPathResult.FIRST_ORDERED_NODE_TYPE,a),
-                   f.src="http://xkcd.mscha.org/frame/random")},
+        0===e?b(a,"#859900"): /* If e is 0 their postcount is an exact multiple
+                                 of 100; show it in bright olive green (?) */
+         e>=97?b(a,"#dc322f"): /* Coming soon, show in slightly-subdued red */
+           3>=e&&b(a,"#268bd2"), /* round-number happenned recently, show in teal */
+        /* If post count is 1190, or a few other values special to mrob27, replace
+           their avatar with a random frame of Time */
+        (27===d||143===d||1011===d||1190===d)
+           &&(f=c('ancestor::*[@class="postprofile"]//img[@alt="User avatar"]',
+                    XPathResult.FIRST_ORDERED_NODE_TYPE,a),
+              f.src="http://xkcd.mscha.org/frame/random")},
+
   b(function(){
     var a,b,e,f;
     for(a=c('//*[@class="postprofile"]//*[.="Posts:"]', XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE),
         b=0,e=a.snapshotLength; e>b;++b)
       f=b,
     d(a.snapshotItem(f).nextSibling)
-  }
-)}).call(this);
+    }
+  )
+}).call(this);
