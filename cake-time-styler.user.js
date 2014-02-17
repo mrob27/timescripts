7da2fdb5345eaeb5caa4e9f3bd88b6e31a4abd9f
Robert Munafo <mrob27@gmail.com>
2014-02-17 09:58:26 -0500

    Clean up formatting

--- cake-time-styler.user.js
+++ cake-time-styler.user.js
@@ -1,7 +1,6 @@
-/*!
 // ==UserScript==
 // @namespace time.aasg.name
-// @name Cake Time Styler
+// @name cake-time-styler for OTT
 // @version 6459.51
 // @description Restyle the post count of people nearby Cake Time
 // @author Aluísio Augusto Silva Gonçalves <aasg@chirpingmustard.com>
@@ -23,7 +22,6 @@
 //   also cause the random Timeframe to appear.
 // 6459.51 add xkcd color names to comments (from the survey results at
 //   http://xkcd.com/color/rgb/ )
-*/
 (function(){
   var a,b,c,d;
   a="101043",
@@ -50,14 +48,16 @@
         d=c('ancestor::*[@class="post"]',XPathResult.FIRST_ORDERED_NODE_TYPE,a),
         d.style.border="2px dotted "+b},
         d=+a.textContent, /* Post count */
-        e=d%100, /* Post count modulo 100, i.e. the last 2 digits of the post count */
+        e=d%100, /* Post count modulo 100, i.e. the last 2 digits of the
+                    post count */
         a=a.parentElement,
         0===e?b(a,"#859900"): /* If e is 0 their postcount is an exact multiple
                                  of 100; show it in baby poop green */
-         e>=97?b(a,"#dc322f"): /* Coming soon, show in tomato red */
-           3>=e&&b(a,"#268bd2"), /* round-number happened recently, show in water blue */
-        /* If post count is 1190, or a few other values special to mrob27, replace
-           their avatar with a random frame of Time */
+        e>=97?b(a,"#dc322f"): /* Coming soon, show in tomato red */
+        3>=e&&b(a,"#268bd2"), /* round-number happened recently, show in
+                                  water blue */
+        /* If post count is 1190, or a few other values special to mrob27,
+           replace their avatar with a random frame of Time */
         (27===d||143===d||1011===d||1190===d)
            &&(f=c('ancestor::*[@class="postprofile"]//img[@alt="User avatar"]',
                     XPathResult.FIRST_ORDERED_NODE_TYPE,a),
@@ -65,7 +65,8 @@
 
   b(function(){
     var a,b,e,f;
-    for(a=c('//*[@class="postprofile"]//*[.="Posts:"]', XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE),
+    for(a=c('//*[@class="postprofile"]//*[.="Posts:"]',
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE),
         b=0,e=a.snapshotLength; e>b;++b)
       f=b,
     d(a.snapshotItem(f).nextSibling)
