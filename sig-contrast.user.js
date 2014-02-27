fbb82c3b235887f538a7616f71450ec7217152a1
Robert Munafo <mrob27@gmail.com>
2014-02-27 12:43:35 -0500

    Change title of one option

--- sig-contrast.user.js
+++ sig-contrast.user.js
@@ -3,7 +3,7 @@
 // @name sig-contrast for OTT
 // @description  Hides signatures and/or make them easily distinguishable
 // @author Robert Munafo
-// @version 8162.47
+// @version 8270.66
 // @downloadURL http://mrob.com/time/scripts-beta/sig-contrast.user.js
 // @include http://forums.xkcd.com/viewtopic.php*
 // @include http://fora.xkcd.com/viewtopic.php*
@@ -15,6 +15,7 @@
 // REVISION HISTORY:
 // np7858.45 first version, based mainly on no-tinytext
 // np8162.47 Place checkboxes below the 'WHO IS ONLINE' bit
+// np8270.66 Change title of one option
 
 sig_contrast = {
 
@@ -31,13 +32,7 @@ sig_contrast = {
 
   // Set a checkbox to be on or off
   setChkVal: function(chk, val) {
-    if (val == 0) {
-      chk.checked=false;
-      console.info('setting checkbox false');
-    } else {
-      chk.checked=true;
-      console.info('setting checkbox true');
-    }
+    chk.checked = (val != 0);
   },
 
   /* opt_action will set or clear an option. */
@@ -45,13 +40,7 @@ sig_contrast = {
     // If the option was just initialized, it will now become set.
     // This makes sense becuse if they've just installed the script
     // and click on the checkbox, they want to set the checkbox.
-    console.info('foo1 ' + nam + ' val is ' + optobj.val);
-    if (optobj.val == 0) {
-      optobj.val = 1;
-    } else {
-      optobj.val = 0;
-    }
-    console.info('foo2 tog to ' + optobj.val);
+    optobj.val = (optobj.val == 0) ? 1 : 0;
     
     this.setChkVal(chk, optobj.val);
     chk.disabled = false;
@@ -59,6 +48,24 @@ sig_contrast = {
     /* Save the user's work in a way that will persist across page loads.
      * see http://wiki.greasespot.net/GM_setValue */
     GM_setValue(nam, JSON.stringify(optobj.val));
+  },
+
+  make_checkbox: function(nam, title, optobj, pdiv) {
+    // Make the checkbox for 'reveal light text'
+    var chk = document.createElement('input');
+    chk.type = 'checkbox';
+    chk.value = 'sigo-' + nam;
+    chk.id = nam;
+
+    var lbl = document.createElement('label')
+    lbl.htmlFor = nam;
+    var lab_text = document.createTextNode(title);
+    lbl.appendChild(lab_text);
+
+    this.setChkVal(chk, optobj.val);
+    chk.addEventListener('click',
+                              this.opt_action.bind(this, chk, optobj, nam));
+    pdiv.appendChild(chk); pdiv.appendChild(lbl);
   },
 
   /* create_checkboxen will add a checkbox that changes an option */
@@ -78,39 +85,12 @@ sig_contrast = {
     opts_div.style.textAlign = 'left';
 
     // Make the checkbox for 'Green background'
-    var chk1 = document.createElement('input');
-    chk1.type = 'checkbox';
-    chk1.value = 'temp-opt1';
-    chk1.id = 'opt1';
-
-    var lbl1 = document.createElement('label')
-    lbl1.htmlFor = "opt1";
-    var lab_text = document.createTextNode('Green background');
-    lbl1.appendChild(lab_text);
-
-    this.setChkVal(chk1, this.opt1.val);
-    chk1.addEventListener('click',
-                this.opt_action.bind(this, chk1, this.opt1, 'opt1'));
+    opts_div.appendChild(document.createTextNode("　　"));
+    this.make_checkbox('opt1', 'Green background', this.opt1, opts_div);
 
     // Make the checkbox for 'Hidden'
-    var chk2 = document.createElement('input');
-    chk2.type = 'checkbox';
-    chk2.value = 'temp-opt2';
-    chk2.id = 'opt2';
-    
-    var lbl2 = document.createElement('label')
-    lbl2.htmlFor = "opt2";
-    var lab2_text = document.createTextNode('Hidden');
-    lbl2.appendChild(lab2_text);
-    
-    this.setChkVal(chk2, this.opt2.val);
-    chk2.addEventListener('click',
-                this.opt_action.bind(this, chk2, this.opt2, 'opt2'));
-
-    opts_div.appendChild(document.createTextNode("　　")); // CJK space, for indentation
-    opts_div.appendChild(chk1); opts_div.appendChild(lbl1);
     opts_div.appendChild(document.createTextNode("　"));
-    opts_div.appendChild(chk2); opts_div.appendChild(lbl2);
+    this.make_checkbox('opt2', 'Hide Signatures', this.opt2, opts_div);
 
     container.appendChild(preDiv);
     container.appendChild(opts_div);
