88606d2448b12523e80bd39212ff3551553b60eb
Robert Munafo <mrob27@gmail.com>
2016-04-22 02:42:49 -0400

    fix syntax errors and bogus "expected assignment" complaints from Tampermonkey

--- cake-time-styler.user.js
+++ cake-time-styler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @namespace http://mrob.com/time/scripts-beta
 // @name cake-time-styler for OTT
-// @version 26397.23
+// @version 27099.67
 // @description Restyle the post count of people nearby Cake Time
 // @author Aluísio Augusto Silva Gonçalves <aasg@chirpingmustard.com>
 // @downloadURL http://mrob.com/time/scripts-beta/cake-time-styler.user.js
@@ -24,62 +24,68 @@
 //   http://xkcd.com/color/rgb/ )
 // 17421.01 add 9111 to the sequence of postcounts resulting in a *Time* frame avatar.
 // 26397.23 handle new version of phpBB on the fora server
+// 27099.67 fix syntax errors and bogus "expected assignment" complaints from Tampermonkey
 //
+console.info("cts1");
 (function(){
   var a,b,c,d;
-  a="101043",
+  a="101043";
 
   b=function(b){
     var c;
-    c=document.querySelector("#topic-search input[name=t]").value,c===a&&b()
-  },
+    c=document.querySelector("#topic-search input[name=t]").value;
+    if (c===a) { b(); }
+  };
 
   c=function(a,b,c){
     var d;
-    return null==c&&(c=document),
-      a=a.replace(/@class="(.+?)"/g,
-            'contains(concat(" ", normalize-space(@class), " "), " $1 ")'),
-      d=document.evaluate(a,c,null,b,null),
-      b===XPathResult.FIRST_ORDERED_NODE_TYPE?d.singleNodeValue:d},
+      console.info("c(" + a + ", " + b + ", " + c + ")");
+    if (null===c) { c=document; }
+    a=a.replace(/@class="(.+?)"/g, 'contains(concat(" ", normalize-space(@class), " "), " $1 ")');
+    d=document.evaluate(a,c,null,b,null);
+    return (b===XPathResult.FIRST_ORDERED_NODE_TYPE ? d.singleNodeValue : d);
+  };
 
   d=function(a){
-    var b,d,e,f;
+    var b,d,e,f,p;
     b=function(a,b){
       var d;
-      return a.style.fontWeight="bold",
-        a.style.color=b,
-        d=c('ancestor::*[@class="post"]',XPathResult.FIRST_ORDERED_NODE_TYPE,a),
-        d.style.border="2px dotted "+b
-    },
-    d=
-      +a.textContent, /* Post count */
-      e=d%100, /* Post count modulo 100, i.e. the last 2 digits of the
+        console.info("b (" + a + ", " + b + ")");
+      a.style.fontWeight="bold";
+      a.style.color=b;
+      d=c('ancestor::*[@class="post"]',XPathResult.FIRST_ORDERED_NODE_TYPE,a);
+      d.style.border="2px dotted "+b;
+    };
+    d=a.textContent; /* Post count */
+    e=d%100; /* Post count modulo 100, i.e. the last 2 digits of the
                   post count */
-      a=a.parentElement,
-      0===e?b(a,"#859900"): /* If e is 0 their postcount is an exact multiple
-                               of 100; show it in baby poop green */
-      e>=97?b(a,"#dc322f"): /* Coming soon, show in tomato red */
-      3>=e&&b(a,"#268bd2"), /* round-number happened recently, show in
-                                water blue */
+    console.info("a = " + a + ", d = " + d + ", e = " + e);
+    p=a.parentElement;
+    if (e === 0) { b(a,"#859900"); } /* If e is 0 their postcount is an exact multiple
+                                        of 100; show it in baby poop green */
+    else if (e >= 97) { b(a,"#dc322f"); } /* Coming soon, show in tomato red */
+    else if (e <= 3) { b(a,"#268bd2"); } /* round-number happened recently, show in
+                                            water blue */
+    else if (d == -1987) { b(a,"#F00"); } /* for debugging */
       /* If post count is 1190, or a few other values special to mrob27,
          replace their avatar with a random frame of Time */
-      (27===d||143===d||1011===d||1190===d||9111===d)
-         &&(f=c('ancestor::*[@class="postprofile"]//img[@alt="User avatar"]',
-                  XPathResult.FIRST_ORDERED_NODE_TYPE,a),
-            f.src="http://xkcd.mscha.org/frame/random")
-  },
+    else if (d===27||d===143||d===1011||d===1190||d===9111) {
+      f=c('ancestor::*[@class="postprofile"]//img[@alt="User avatar"]', XPathResult.FIRST_ORDERED_NODE_TYPE, a);
+      f.src="http://xkcd.mscha.org/frame/random";
+    }
+  };
 
   b(
     function(){
-      var a,b,e,f;
-      for(a=c('//*[@class="postprofile"]//*[.="Posts:"]',
-          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE), b=0, e=a.snapshotLength;
-          e>b;
+      var a,b,e;
+      for(b=0,
+          a=c('//*[@class="postprofile"]//*[.="Posts:"]', XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE),
+          e=a.snapshotLength;
+          b<e;
           ++b)
       {
-        f=b;
-        d(a.snapshotItem(f).nextSibling.nextSibling)
+        d(a.snapshotItem(b).nextSibling.nextSibling);
       }
     }
-  )
+  );
 }).call(this);
